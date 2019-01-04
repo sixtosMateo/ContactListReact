17cfb43d23b9b45f9a9490be83b743ff4e25c344
mateosixtos <sixtosmateo@gmail.com>
2019-01-03 17:19:37 -0800

    moved my contacts to state property

--- contactListProject/contactlist/src/App.js
+++ contactListProject/contactlist/src/App.js
@@ -2,33 +2,35 @@ import React, { Component } from 'react';
 import ContactLists from './ContactLists';
 import * as ContactsAPI from './utils/ContactsAPI';
 
-const contacts = [
-  {
-    "id": "ryan",
-    "name": "Ryan Florence",
-    "email": "ryan@reacttraining.com",
-    "avatarURL": "http://localhost:5001/ryan.jpg"
-  },
-  {
-    "id": "michael",
-    "name": "Michael Jackson",
-    "email": "michael@reacttraining.com",
-    "avatarURL": "http://localhost:5001/michael.jpg"
-  },
-  {
-    "id": "tyler",
-    "name": "Tyler McGinnis",
-    "email": "tyler@reacttraining.com",
-    "avatarURL": "http://localhost:5001/tyler.jpg"
-  }
-]
-
 
 class App extends Component {
+  state={
+    contacts : [
+      {
+        "id": "ryan",
+        "name": "Ryan Florence",
+        "email": "ryan@reacttraining.com",
+        "avatarURL": "http://localhost:5001/ryan.jpg"
+      },
+      {
+        "id": "michael",
+        "name": "Michael Jackson",
+        "email": "michael@reacttraining.com",
+        "avatarURL": "http://localhost:5001/michael.jpg"
+      },
+      {
+        "id": "tyler",
+        "name": "Tyler McGinnis",
+        "email": "tyler@reacttraining.com",
+        "avatarURL": "http://localhost:5001/tyler.jpg"
+      }
+    ]
+
+  }
   render() {
     return (
       <div>
-        <ContactLists contacts={contacts}/>
+        <ContactLists contacts={this.state.contacts}/>
       </div>
     )
   }
