099680ce7bc0e2cc2fb6e2589e0b23afb3a452e0
mateosixtos <sixtosmateo@gmail.com>
2019-01-04 12:59:30 -0800

    installed PropTypes pkg and established them in Contact List Component file

--- contactListProject/contactlist/src/App.js
+++ contactListProject/contactlist/src/App.js
@@ -35,7 +35,8 @@ class App extends Component {
       contacts: state.contacts.filter((c) => c.id !== contact.id)
     }))
   }
-  
+
+
   render() {
     return (
       <div>
--- contactListProject/contactlist/src/ContactLists.js
+++ contactListProject/contactlist/src/ContactLists.js
@@ -1,30 +1,68 @@
-import React, {Component} from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 
-class ContactLists extends Component {
-  // <button classnName = contact-remove>
-        // passing onDeleteContact method from parent component(app)
 
-  render(){
-    return(
-      <ol className='contact-list'>
-          {this.props.contacts.map((contact)=> (
-              <li key={contact.id} className='contact-list-item'>
-                <div className='contact-avatar'
-                style={{backgroundImage: `url(${contact.avatarURL})`}}>
-                </div>
-                <div className='contact-details'>
-                  <p>{contact.name}</p>
-                  <p>{contact.email}</p>
-                </div>
+// this is a example of stateless functional component
+function ContactLists(props){
+  return(
+    <ol className='contact-list'>
+              {props.contacts.map((contact)=> (
+                  <li key={contact.id} className='contact-list-item'>
+                    <div className='contact-avatar'
+                    style={{backgroundImage: `url(${contact.avatarURL})`}}>
+                    </div>
+                    <div className='contact-details'>
+                      <p>{contact.name}</p>
+                      <p>{contact.email}</p>
+                    </div>
 
-                <button className='contact-remove'onClick={() => this.props.onDeleteContact(contact)} >
-                  remove
-                </button>
-              </li>
-            ))
-          }
+                    <button className='contact-remove'onClick={() => props.onDeleteContact(contact)} >
+                      remove
+                    </button>
+                  </li>
+                ))
+              }
       </ol>
-    )
-  }
+  )
+
+
+
 }
+
+// set a property to our statelss functional component
+ContactLists.propTypes={
+  contacts: PropTypes.array.isRequired,
+  onDeleteContact: PropTypes.func.isRequired
+
+}
+
+//
+// class ContactLists extends Component {
+//   // <button classnName = contact-remove>
+//         // passing onDeleteContact method from parent component(app)
+//
+//   render(){
+//     return(
+//       <ol className='contact-list'>
+//           {this.props.contacts.map((contact)=> (
+//               <li key={contact.id} className='contact-list-item'>
+//                 <div className='contact-avatar'
+//                 style={{backgroundImage: `url(${contact.avatarURL})`}}>
+//                 </div>
+//                 <div className='contact-details'>
+//                   <p>{contact.name}</p>
+//                   <p>{contact.email}</p>
+//                 </div>
+//
+//                 <button className='contact-remove'onClick={() => this.props.onDeleteContact(contact)} >
+//                   remove
+//                 </button>
+//               </li>
+//             ))
+//           }
+//       </ol>
+//     )
+//   }
+// }
+
 export default ContactLists;
