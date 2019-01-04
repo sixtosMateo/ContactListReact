7923bdf2b5e82a3af420a15f9f4aa6fd26821dad
mateosixtos <sixtosmateo@gmail.com>
2019-01-04 13:57:06 -0800

    install two pkg to help filter our contact list and filter contact list

--- contactListProject/contactlist/src/ContactLists.js
+++ contactListProject/contactlist/src/ContactLists.js
@@ -1,5 +1,7 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
+import escapeRegExp from 'escape-string-regexp';
+import sortBy from 'sort-by'
 
 //
 // // this is a example of stateless functional component
@@ -58,10 +60,21 @@ class ContactLists extends Component {
   }
 
   render(){
+    let showingContacts
+    if(this.state.query){
+      const match = new RegExp(escapeRegExp(this.state.query), 'i')
+      showingContacts = this.props.contacts.filter((contact) => match.test(contact.name))
+    }
+    else{
+      showingContacts = this.props.contacts
+    }
+
+    showingContacts.sort(sortBy('name'))
+
     return(
       <div className='contact-container'>
         {JSON.stringify(this.state)}
-        
+
         <div className='contact-search'>
           <input className='contact-search-box'
                  type='text'
@@ -71,7 +84,7 @@ class ContactLists extends Component {
         </div>
 
         <ol className='contact-list'>
-            {this.props.contacts.map((contact)=> (
+            {showingContacts.map((contact)=> (
                 <li key={contact.id} className='contact-list-item'>
                   <div className='contact-avatar'
                   style={{backgroundImage: `url(${contact.avatarURL})`}}>
