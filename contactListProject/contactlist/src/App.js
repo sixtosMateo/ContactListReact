7fa310935c3138ab80cc63026c79507e894845cb
mateosixtos <sixtosmateo@gmail.com>
2019-01-04 00:14:41 -0800

    using functional setState method to delete contact

--- contactListProject/contactlist/src/App.js
+++ contactListProject/contactlist/src/App.js
@@ -27,10 +27,19 @@ class App extends Component {
     ]
 
   }
+
+  removeContact = (contact) => {
+    // using function setState because previous state needed to remove contact
+    this.setState((state) => ({
+      // setting array contacts to previous state BUT filtered JS function to remove contact
+      contacts: state.contacts.filter((c) => c.id !== contact.id)
+    }))
+  }
+  
   render() {
     return (
       <div>
-        <ContactLists contacts={this.state.contacts}/>
+        <ContactLists onDeleteContact = {this.removeContact} contacts={this.state.contacts}/>
       </div>
     )
   }
--- contactListProject/contactlist/src/ContactLists.js
+++ contactListProject/contactlist/src/ContactLists.js
@@ -1,6 +1,9 @@
 import React, {Component} from 'react';
 
 class ContactLists extends Component {
+  // <button classnName = contact-remove>
+        // passing onDeleteContact method from parent component(app)
+
   render(){
     return(
       <ol className='contact-list'>
@@ -13,7 +16,8 @@ class ContactLists extends Component {
                   <p>{contact.name}</p>
                   <p>{contact.email}</p>
                 </div>
-                <button className='contact-remove'>
+
+                <button className='contact-remove'onClick={() => this.props.onDeleteContact(contact)} >
                   remove
                 </button>
               </li>
